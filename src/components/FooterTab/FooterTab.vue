63731109f0da41c71cc69904a8da5272cfc93e13
cgw <289929553@qq.com>
2017-05-12 15:26:34 +0800

    再修改一下底部

--- src/components/FooterTab/FooterTab.vue
+++ src/components/FooterTab/FooterTab.vue
@@ -1,13 +1,10 @@
 <template>
     <div class="footer">
-        <footer class="fixBottomBox">
-            <ul>
-                <router-link active-class='menu-active' tag="li" :to="menu.path" class="tabItem" v-for="menu in menuData">
-                    <i :class="menu.code"></i>
-                    <a>{{menu.text}}</a>
-                </router-link>
-            </ul>
-        </footer>
+        <div class="footer">
+            <router-link :active-class="menu.code+'-active'" tag="div" :to="menu.path" class="item" :class="menu.code" v-for="menu in menuData">
+               {{menu.text}}
+            </router-link>
+        </div>
     </div>
 </template>
 
@@ -34,7 +31,6 @@ export default {
         };
     },
     methods: {
-
     },
 
     watch: {
@@ -45,90 +41,57 @@ export default {
 </script>
 
 <style lang="scss" socped>
-.footer-tab {
-    background-color: #e3e1e1;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    position: fixed;
-    z-index: 1;
-    text-align: center;
+.footer {
     display: flex;
-    div {
-        display: block;
-        flex: 1;
-        height: 6rem;
-        i {
-            display: block;
-            width: 60px;
-            height: 60px;
-            margin: 1px auto 5px;
-            background-color: #000;
-        }
-        i.home {
-            // background: url(../../assets/icons.png) -45px -66px no-repeat;
-        }
-        i.classification {
-            // background: url(../../assets/icons.png) -658px -851px no-repeat;
-        }
-        i.shoppingcart {
-            // background: url(../../assets/icons.png) -530px -459px no-repeat;
-        }
-    }
-    .menu-active {
-        background-color: #a9a9dc;
-    }
-}
-
-.fixBottomBox {
-    width: 100%;
     height: 50px;
-    box-shadow: 0 -2px 10px #ccc;
     position: fixed;
     bottom: 0;
-    left: 0;
-    background-color: #fff;
-    ul {
-        display: flex;
-        justify-content: space-around;
+    width: 100%;
+    border-top: 1px solid #ddd;
+    background: #fff;
+    .item {
+      width: 100%;
+      height: 100%;
+      line-height: 75px;
+      color: #888;
+      font-size: 12px;
+      text-align: center;
+      &.home {
+        background: url("../../assets/images/svg/footer_home1.svg") no-repeat center 25%;
+        background-size: 20px;
+      }
+      &.home-active {
+        color: #ff6700;
+        background: url("../../assets/images/svg/footer_home.svg") no-repeat center 25%;
+        background-size: 20px;
+      }
+      &.classification {
+        background: url("../../assets/images/svg/footer_lei1.svg") no-repeat center 25%;
+        background-size: 20px;
+      }
+      &.classification-active {
+        color: #ff6700;
+        background: url("../../assets/images/svg/footer_lei.svg") no-repeat center 25%;
+        background-size: 20px;
+      }
+      &.shoppingcart {
+        background: url("../../assets/images/svg/footer_cart1.svg") no-repeat center 25%;
+        background-size: 20px;
+      }
+      &.shoppingcart-active {
+        color: #ff6700;
+        background: url("../../assets/images/svg/footer_cart.svg") no-repeat center 25%;
+        background-size: 20px;
+      }
+      &.me {
+        background: url("../../assets/images/svg/footer_me1.svg") no-repeat center 25%;
+        background-size: 20px;
+      }
+      &.me-active {
+        color: #ff6700;
+        background: url("../../assets/images/svg/footer_me.svg") no-repeat center 25%;
+        background-size: 20px;
+      }
     }
-    .tabItem {
-        width: 18%;
-        height: 100%;
-        text-align: center;
-        i {
-            display: block;
-            width: 35px;
-            height: 30px;
-            margin: 1px auto 1px;
-            background-color: #000;
-        }
-        i.home {
-            background: url(../../assets/images/footbar-home.png) 0 0 no-repeat;
-        }
-        i.classification {
-            background: url(../../assets/images/footbar-classification.png) 0 0 no-repeat;
-        }
-        i.shoppingcart {
-            background: url(../../assets/images/footbar-shoppingcart.png) 0 0 no-repeat;
-        }
-        a {
-            color: #5d5f6a;
-        }
-    }
-    .menu-active {
-        i.home {
-            background: url(../../assets/images/footbar-home-active.png) 0 0 no-repeat;
-        }
-        i.classification {
-            background: url(../../assets/images/footbar-classification-active.png) 0 0 no-repeat;
-        }
-        i.shoppingcart {
-            background: url(../../assets/images/footbar-shoppingcart-active.png) 0 0 no-repeat;
-        }
-        a {
-            color: #ec6149;
-        }
-    }
-}
+  }
 </style>
