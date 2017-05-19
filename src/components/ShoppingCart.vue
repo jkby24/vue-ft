5de23556d09105f5659371dfbea8f1d0b2a3392d
cgw <289929553@qq.com>
2017-05-19 19:25:54 +0800

    cart底部

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
-    <ul>
-      <li v-for="p in commodities">
-        {{ p.commodity_name }} - {{ p.price | currency }} x {{ p.quantity }}
-      </li>
-    </ul>
-    <p>Total: {{ total | currency }}</p>
+    <div class="cart-main">
+      <ul>
+        <li v-for="p in commodities">
+          {{ p.commodity_name }} - {{ p.price | currency }} x {{ p.quantity }}
+        </li>
+      </ul>
+    </div>
+    <div class="cart-footer">
+      <p>Total: {{ total | currency }}</p>
+      <div class="bottom-submit ui-box">
+        <div class="price">
+          <span>共2件 金额：</span>
+          <br>
+          <strong>138.00</strong>
+          <span>元</span>
+        </div>
+        <div class="btn">
+          <a class="ui-button ui-button-disable" href="/#/product/category" data-stat-id="97e777a98f9ebbc7" onclick="_msq.push(['trackEvent', '221a86c5a4688b35-97e777a98f9ebbc7', '/#/product/category', 'pcpid', '']);">
+            <span>继续购物</span>
+          </a>
+        </div>
+        <div class="btn">
+          <a class="ui-button" data-stat-id="9727a6cc23d1b9eb" onclick="_msq.push(['trackEvent', '221a86c5a4688b35-9727a6cc23d1b9eb', '', 'pcpid', '']);">
+            <span>去结算</span>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,7 +44,7 @@ export default {
       commodities: 'cartCommodities',
       checkoutStatus: 'checkoutStatus'
     }),
-    total () {
+    total() {
       return this.commodities.reduce((total, p) => {
         return total + p.price * p.quantity
       }, 0)
